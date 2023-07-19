476eeb91a77513768182d974351a6d382e250bf7
gushiii <1726143054@qq.com>
2023-07-19 20:41:43 +0800

    create project

---
+++ src/main/java/com/gushi/App.java
@@ -0,0 +1,13 @@
+package com.gushi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
